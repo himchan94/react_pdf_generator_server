f8d3d0e38e213450206cf2d31155b244f4d4320f
himchan94 <himchan9494@gmail.com>
2022-04-03 19:40:29 +0900

    feat: Created client view

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState({
+    name: "",
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  });
+
+  const handleChange = ({ target: { value, name } }) => {
+    setData({ [name]: value });
+  };
+
+  const createAndDownloadPdf = () => {
+    axios
+      .post("/create-pdf", data)
+      .then(() => axios.get("/fetch-pdf", { responseType: "blob" }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
+  };
+
+  return (
+    <div className='App'>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        onChange={handleChange}
+      />
+      <input
+        type='number'
+        placeholder='Receipt ID'
+        name='receiptId'
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        placeholder='Price 1'
+        name='price1'
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        placeholder='Price 2'
+        name='price2'
+        onChange={handleChange}
+      />
+      <button onClick={createAndDownloadPdf}>Download PDF</button>
+    </div>
+  );
+}
+
+export default App;
